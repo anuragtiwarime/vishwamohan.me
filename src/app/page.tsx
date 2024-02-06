fa0e87c5340c46dbdc0752a0e4ed7ab0f6d5c5b0
Vinay Pratap Singh <technicalharvi@gmail.com>
2024-02-06 16:28:57 +0530

    completed featured and worked with company section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/navbar";
+import { featuredCompanyData, workedWithData } from "@/utils/constants";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -16,6 +18,68 @@ export default function Home() {
             consectetur possimus deleniti atque ratione id quisquam cum aut
             quibusdam. Debitis, placeat nesciunt?
           </p>
+        </div>
+      </section>
+
+      {/* featured on section */}
+      <section className="my-10 space-y-10 px-5 md:px-10">
+        <div className="space-y-5">
+          <h1 className="text-3xl text-center font-semibold">Featured on</h1>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            obcaecati natus consectetur consequuntur maxime deleniti sapiente
+            deserunt, nostrum magni commodi aspernatur vitae nulla, iure
+            voluptates?
+          </p>
+        </div>
+        <div className="flex items-center gap-20 justify-center">
+          {featuredCompanyData.length &&
+            featuredCompanyData.map((company, index) => {
+              return (
+                <div
+                  key={index}
+                  className="relative w-40 h-40 bg-white rounded-full shadow-lg shadow-gray-700"
+                >
+                  <Image
+                    className="object-contain p-2"
+                    src={company?.image}
+                    fill
+                    alt={company.name}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      </section>
+
+      {/* worked with section */}
+      <section className="my-10 space-y-10 px-5 md:px-10">
+        <div className="space-y-5">
+          <h1 className="text-3xl text-center font-semibold">Worked with</h1>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            obcaecati natus consectetur consequuntur maxime deleniti sapiente
+            deserunt, nostrum magni commodi aspernatur vitae nulla, iure
+            voluptates?
+          </p>
+        </div>
+        <div className="flex items-center gap-14 justify-center">
+          {workedWithData.length &&
+            workedWithData.map((company, index) => {
+              return (
+                <div
+                  key={index}
+                  className="relative w-40 h-14 bg-white rounded-md shadow-md shadow-gray-700"
+                >
+                  <Image
+                    className="object-cover px-5 py-2"
+                    src={company?.image}
+                    fill
+                    alt={company.name}
+                  />
+                </div>
+              );
+            })}
         </div>
       </section>
     </main>
---
+++ src/utils/constants.tsx
@@ -0,0 +1,46 @@
+interface IDataType {
+  name: string;
+  image: string;
+}
+
+export const featuredCompanyData: IDataType[] = [
+  {
+    name: "IIT Bombay",
+    image: "/assets/featuredCompany/IIT_Bombay.png",
+  },
+  {
+    name: "IIT Kanpur",
+    image: "/assets/featuredCompany/IIT_Kanpur.png",
+  },
+  {
+    name: "IIT Kharagpur",
+    image: "/assets/featuredCompany/IIT_Kharagpur.png",
+  },
+  {
+    name: "Galgotias",
+    image: "/assets/featuredCompany/galgotias.png",
+  },
+];
+
+export const workedWithData: IDataType[] = [
+  {
+    name: "Linkedin",
+    image: "/assets/workedWith/linkedin.png",
+  },
+  {
+    name: "Walmart",
+    image: "/assets/workedWith/walmart.png",
+  },
+  {
+    name: "Paypal",
+    image: "/assets/workedWith/paypal.png",
+  },
+  {
+    name: "Amazon",
+    image: "/assets/workedWith/amazon.png",
+  },
+  {
+    name: "Oracle",
+    image: "/assets/workedWith/oracle.png",
+  },
+];
